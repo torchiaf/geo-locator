2f61fb3263de51fa21f83ed9ea0c3ea295bbd8a5
Francesco Torchia <francesco.torchia@suse.com>
2023-01-31 11:10:28 +0100

    operator logic
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- controllers/geolocator_controller.go
+++ controllers/geolocator_controller.go
@@ -17,20 +17,42 @@ limitations under the License.
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"context"
 
+	appsv1 "github.com/torchiaf/geo-locator/api/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
-
-	appsv1 "github.com/torchiaf/geo-locator/api/v1"
 )
 
 // GeoLocatorReconciler reconciles a GeoLocator object
 type GeoLocatorReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+}
+
+type NodeLocation struct {
+	Query       string  `json:"query"`
+	Status      string  `json:"status,omitempty"`
+	Country     string  `json:"country,omitempty"`
+	CountryCode string  `json:"countryCode,omitempty"`
+	Region      string  `json:"region,omitempty"`
+	RegionName  string  `json:"regionName,omitempty"`
+	City        string  `json:"city,omitempty"`
+	Zip         string  `json:"zip,omitempty"`
+	Lat         float32 `json:"lat,omitempty"`
+	Lon         float32 `json:"lon,omitempty"`
+	Timezone    string  `json:"timezone,omitempty"`
+	Isp         string  `json:"isp,omitempty"`
+	Org         string  `json:"org,omitempty"`
+	As          string  `json:"as,omitempty"`
 }
 
 //+kubebuilder:rbac:groups=apps.suse.com,resources=geolocators,verbs=get;list;watch;create;update;patch;delete
@@ -40,6 +62,9 @@ type GeoLocatorReconciler struct {
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
+
+// compare nodes to see if labels is null, then update
+
 // the GeoLocator object against the actual cluster state, and then
 // perform operations to make the cluster state reflect the state specified by
 // the user.
@@ -47,9 +72,50 @@ type GeoLocatorReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *GeoLocatorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	Log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	nodes := &corev1.NodeList{}
+	err := r.Client.List(ctx, nodes)
+
+	if err != nil {
+		Log.Error(err, "Failed to get Nodes list")
+		return ctrl.Result{}, err
+	}
+
+	for i := 0; i < len(nodes.Items); i++ {
+		node := nodes.Items[i]
+		address := node.Status.Addresses[0].Address
+
+		infoMessage := fmt.Sprintf("Node Ip found: %s", address)
+		fmt.Println(infoMessage) // TODO(user): remove
+		Log.Info(infoMessage)
+
+		// See https://ip-api.com
+		resp, err := http.Get(`http://ip-api.com/json/`)
+
+		if err != nil {
+			Log.Error(err, "Failed to get Node position")
+			return ctrl.Result{}, err
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+
+		var location NodeLocation
+		if err := json.Unmarshal(body, &location); err != nil {
+			fmt.Println("Can not unmarshal JSON position")
+			return ctrl.Result{}, err
+		}
+
+		node.Annotations["geo-locator.country"] = location.Country
+		node.Annotations["geo-locator.region"] = location.RegionName
+		node.Annotations["geo-locator.city"] = location.City
+
+		if err := r.Update(ctx, &node); err != nil {
+			Log.Error(err, "unable to update Node")
+			return ctrl.Result{}, err
+		}
+
+	}
 
 	return ctrl.Result{}, nil
 }
